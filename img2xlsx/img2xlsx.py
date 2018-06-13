f9ffcd3d0e1490182fcf81b9ddfe46640da9d0c1
yorozuone <yorozuone@users.noreply.github.com>
2018-06-13 11:56:36 +0900

    .

--- img2xlsx/img2xlsx.py
+++ img2xlsx/img2xlsx.py
@@ -3,12 +3,13 @@ import openpyxl as px
 from openpyxl.drawing.image import Image
 from datetime import datetime, date
 
-def cap(src, dst):
+def cap(src_folder, dst_folder, name, ext):
     wb = px.Workbook()
     ws = wb.active
-    img = Image(src)
-    ws.add_image(img, 'A1')
-    wb.save(dst)
+    img = Image(src_folder + name + ext)
+    ws['A1'].value = name
+    ws.add_image(img, 'A2')
+    wb.save(dst_folder + name + '.xlsx')
 
 src_folder = os.getcwd() + '\\'
 dst_folder = os.getcwd() + '\\' + datetime.now().strftime("%Y%m%d%H%M%S") + '\\'
@@ -16,9 +17,8 @@ dst_folder = os.getcwd() + '\\' + datetime.now().strftime("%Y%m%d%H%M%S") + '\\'
 os.mkdir(dst_folder)
 
 src_files = os.listdir(src_folder)
-print(src_files)
 for src_file in src_files:
     if os.path.isfile(src_folder + src_file):
         name, ext = os.path.splitext(os.path.basename(src_file))
         if ext in {'.jpg', '.png'}:
-            cap(src_folder + name + ext, dst_folder + name + '.xlsx')
+            cap(src_folder, dst_folder, name, ext)
